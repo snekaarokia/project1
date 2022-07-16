6f09260e102be60859d7d308d65d55f36979391a
9578686280 <aswinidmerode@gmail.com>
2022-07-16 15:20:32 +0530

    testing

---
+++ ifex.java
@@ -0,0 +1,26 @@
+
+//made correction...
+
+import java.util.Scanner;
+
+
+
+class ifex
+{
+public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+int n;
+System.out.println("Enter a number");
+n=sc.nextInt();
+if(n>0)
+{
+System.out.println("true");
+}
+else
+{
+System.out.println("false");
+}
+
+}
+}
