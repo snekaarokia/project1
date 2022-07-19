49d2dd0169bb9e5456fcfbeaaccac087b003bce3
snekaarokia <madhankumar.thirumoorthy@gmail.com>
2022-07-19 11:24:19 +0530

    checking

---
+++ ar2.java
@@ -0,0 +1,37 @@
+
+//github...
+import java.util.*;
+class ar2
+{
+public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+int a[]=new int[8];
+System.out.println("enter the array");
+
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+}
+System.out.println("the arrays are");
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+
+Arrays.sort(a);
+System.out.println("the sorted array is");
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+
+}
+}
+
+
+
+
+
+
+
