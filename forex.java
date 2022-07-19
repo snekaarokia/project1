ed75a207da5a80b49425d126b59fb4a9f77b17f9
snekaarokia <madhankumar.thirumoorthy@gmail.com>
2022-07-19 11:57:33 +0530

    modify

---
+++ forex.java
@@ -0,0 +1,18 @@
+// modify......
+import java.util.Scanner;
+class forex
+{
+public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the number: ");
+int n=sc.nextInt();
+int i=1;
+do
+{
+System.out.println(i);
+i++;
+}
+while(i<=n);
+}
+}
