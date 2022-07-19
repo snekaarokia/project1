9fe4d2a9a239cd988030b42defc9fb9692bd8ddc
snekaarokia <madhankumar.thirumoorthy@gmail.com>
2022-07-19 12:20:36 +0530

    add value

---
+++ nest.java
@@ -0,0 +1,25 @@
+//add num.......
+/*
+12345
+1234
+123
+12
+1 
+
+*/
+
+class nest
+{
+public static void main(String ar[])
+{
+for(int i=5;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+
+}
+}
